d06ef6962a2c87698fd3b742cb307c460f2ff11e
toki <toki@d28d52c5-2bfd-4296-a7a2-2b3bd35462a5>
2007-06-16 16:13:47 +0000

    *** empty log message ***
    git-svn-id: file:///home/toki/svn_root/gluon/gluon/trunk@59 d28d52c5-2bfd-4296-a7a2-2b3bd35462a5

--- run/lib/Example/Foreach.rb
+++ run/lib/Example/Foreach.rb
@@ -1,5 +1,28 @@
 class Example
   class Foreach
+    class User
+      def initialize(name, age)
+        @name = name
+        @age = age
+      end
+
+      attr_reader :name
+      attr_reader :age
+    end
+
+    def initialize
+      @fruits = %w[ apple banana orange ]
+      @users = [
+        User.new('Taro', 21),
+        User.new('Hanako', 23),
+        User.new('Tanaka', 18)
+      ]
+      @country = 'Japanese'
+    end
+
+    attr_reader :fruits
+    attr_reader :users
+    attr_reader :country
   end
 end
 
