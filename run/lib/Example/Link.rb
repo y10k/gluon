9e7f63e7a0950beecd744bc66c7fafd37251fb0a
toki <toki@d28d52c5-2bfd-4296-a7a2-2b3bd35462a5>
2007-06-17 02:00:27 +0000

    *** empty log message ***
    git-svn-id: file:///home/toki/svn_root/gluon/gluon/trunk@61 d28d52c5-2bfd-4296-a7a2-2b3bd35462a5

--- run/lib/Example/Link.rb
+++ run/lib/Example/Link.rb
@@ -1,5 +1,16 @@
 class Example
   class Link
+    def welcom
+      Welcom
+    end
+
+    def description
+      'return to welcom page.'
+    end
+
+    def ruby_home
+      'http://www.ruby-lang.org/'
+    end
   end
 end
 
