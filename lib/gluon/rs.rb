59cb39cc2c70260a6c01ccee0aa67a9161657189
toki <toki@d28d52c5-2bfd-4296-a7a2-2b3bd35462a5>
2008-02-20 03:42:49 +0000

    * lib/gluon/rs.rb: session manager.
    git-svn-id: file:///home/toki/svn_root/gluon/gluon/trunk@141 d28d52c5-2bfd-4296-a7a2-2b3bd35462a5

--- lib/gluon/rs.rb
+++ lib/gluon/rs.rb
@@ -1,22 +1,210 @@
 # request response
 
+require 'thread'
+require 'digest'
 require 'forwardable'
 
 module Gluon
+  class MemoryStore
+    # for ident(1)
+    CVS_ID = '$Id$'
+
+    def initialize
+      @lock = Mutex.new
+      @store = {}
+    end
+
+    def new_id
+      @lock.synchronize{
+        begin
+          id = yield
+        end while (@store.key? id)
+        id
+      }
+    end
+
+    def save(id, session)
+      @lock.synchronize{
+        @store[id] = {
+          :session => session,
+          :last_modified => Time.now
+        }
+      }
+      nil
+    end
+
+    def load(id)
+      @lock.synchronize{
+        if (entry = @store[id]) then
+          entry[:last_modified] = Time.now
+          return entry[:session]
+        end
+      }
+      nil
+    end
+
+    def delete(id)
+      @lock.synchronize{
+        @store.delete(id)
+      }
+    end
+
+    def expire(alive_time)
+      @lock.synchronize{
+        @store.delete_if{|id, entry|
+          entry[:last_modified] < alive_time
+        }
+      }
+      nil
+    end
+  end
+
+  class SessionNotFoundError < StandardError
+  end
+
+  class SessionManager
+    # for ident(1)
+    CVS_ID = '$Id$'
+
+    def initialize(options={})
+      @default_key = options[:default_key] || 'session_id'
+      @id_max_length = options[:id_max_length] || 32
+      @life_time = options[:life_time] || 60 * 60
+      @auto_expire = (options.key? :auto_expire) ? options[:auto_expire] : true
+      @digest = options[:digest] || Digest::MD5
+      @store = options[:store] || MemoryStore.new
+    end
+
+    attr_reader :default_key
+    attr_reader :id_max_length
+    attr_reader :life_time
+
+    def auto_expire?
+      @auto_expire
+    end
+
+    def create_new_id
+      now = Time.now
+      id = @digest.new
+      id.update(now.to_s)
+      id.update(now.usec.to_s)
+      id.update(rand(0).to_s)
+      id.update($$.to_s)
+      id.update(CVS_ID)
+      id.hexdigest[0, @id_max_length]
+    end
+    private :create_new_id
+
+    def new_id
+      @store.new_id{ create_new_id }
+    end
+
+    def save(id, session)
+      @store.save(id, Marshal.dump(session))
+      nil
+    end
+
+    def load(id)
+      if (session = @store.load(id)) then
+        return Marshal.load(session)
+      end
+      nil
+    end
+
+    def delete(id)
+      @store.delete(id)
+      nil
+    end
+
+    def expire(now=Time.now)
+      @store.expire(now - @life_time)
+      nil
+    end
+
+    def transaction(req, res)
+      handler = SessionHandler.new(self, req, res)
+      begin
+        r = yield(handler)
+        handler.save_all
+      ensure
+        expire if @auto_expire
+      end
+      r
+    end
+  end
+
+  class SessionHandler
+    # for ident(1)
+    CVS_ID = '$Id$'
+
+    extend Forwardable
+
+    def initialize(man, req, res)
+      @man = man
+      @req = req
+      @res = res
+      @sessions = {}
+    end
+
+    def_delegator :@man, :default_key
+
+    def new_session(create=true, options={})
+      key = options[:key] || @man.default_key
+      if (id = @req.cookies[key]) then
+        # nothing to do.
+      elsif (@sessions.key? key) then
+        id = @sessions[key][0]
+      else
+        unless (create) then
+          raise SessionNotFoundError, "not found a session: #{key}"
+        end
+        id = @man.new_id
+      end
+      session = @man.load(id) || {}
+      @sessions[key] = [ id, session, options ]
+      session
+    end
+
+    def delete(key=@man.default_key)
+      id, session = @sessions.delete(key)
+      @man.delete(id) if id
+      nil
+    end
+
+    def save_all
+      for key, (id, session, options) in @sessions
+        @man.save(id, session)
+        if (options.empty?) then
+          @res.set_cookie(key, id)
+        else
+          @res.set_cookie(key, options.dup.update(:value => id))
+        end
+      end
+      nil
+    end
+  end
+
   class RequestResponseContext
     # for idnet(1)
     CVS_ID = '$Id$'
 
     extend Forwardable
 
+    #def initialize(req, res, session, dispatcher)
     def initialize(req, res, dispatcher)
       @req = req
       @res = res
+      #@session = session
       @dispatcher = dispatcher
     end
 
     attr_reader :req
     attr_reader :res
+
+    def_delegator :@session, :new_session
+    def_delegator :@session, :delete, :delete_session
+    def_delegator :@session, :default_key, :default_session_key
+
     def_delegator :@dispatcher, :look_up
     def_delegator :@dispatcher, :class2path
 
--- test/test_rs.rb
+++ test/test_rs.rb
@@ -5,6 +5,146 @@ require 'rack'
 require 'test/unit'
 
 module Gluon::Test
+  class MemoryStoreTest < Test::Unit::TestCase
+    # for ident(1)
+    CVS_ID = '$Id$'
+
+    def setup
+      @store = Gluon::MemoryStore.new
+    end
+
+    def test_save_and_load
+      assert_nil(@store.load('foo'))
+      @store.save('foo', "Hello world.\n")
+      assert_equal("Hello world.\n", @store.load('foo'))
+    end
+
+    def test_delete
+      @store.save('foo', "Hello world.\n")
+      @store.delete('foo')
+      assert_nil(@store.load('foo'))
+    end
+
+    def test_not_delete_other_session
+      @store.save('foo', "Hello world.\n")
+      @store.delete('bar')
+      assert_equal("Hello world.\n", @store.load('foo'))
+    end
+
+    def test_new_id
+      assert_equal('foo', @store.new_id{ 'foo' })
+    end
+
+    def test_new_id_search
+      @store.save('foo', '')
+      @store.save('bar', '')
+      id_list = %w[ foo bar baz ]
+      assert_equal('baz', @store.new_id{ id_list.shift || flunk })
+    end
+
+    def test_expire
+      now = Time.now
+      @store.save('foo', "Hello world.\n")
+
+      @store.expire(now - 1)
+      assert_equal("Hello world.\n", @store.load('foo'))
+
+      @store.expire(now + 1)
+      assert_nil(@store.load('foo'))
+    end
+  end
+
+  class SessionManagerTest < Test::Unit::TestCase
+    # for ident(1)
+    CVS_ID = '$Id$'
+
+    def setup
+      @store = Gluon::MemoryStore.new
+      @man = Gluon::SessionManager.new(:store => @store)
+      @req = Rack::Request.new({})
+      @res = Rack::Response.new
+    end
+
+    def test_new_session
+      @man.transaction(@req, @res) {|handler|
+        session = handler.new_session
+        assert_equal({}, session)
+        session['foo'] = "Hello world.\n"
+      }
+      assert_match(/session_id=\S*/, @res['Set-Cookie'])
+      /session_id=(\S*)/ =~ @res['Set-Cookie'] && id = $1 or flunk('not found a session id')
+      assert_equal({ 'foo' => "Hello world.\n" }, Marshal.load(@store.load(id)))
+    end
+
+    def test_new_session2
+      @man.transaction(@req, @res) {|handler|
+        handler.new_session(true, :key => 'foo')
+        handler.new_session(true, :key => 'bar')
+      }
+
+      foo_cookie = @res['Set-Cookie'].find{|c| c =~ /foo/ } or flunk('not found a session key')
+      /foo=(\S*)/ =~ foo_cookie && foo_id = $1 or flunk('not found a session id')
+      assert_equal({}, Marshal.load(@store.load(foo_id)))
+
+      bar_cookie = @res['Set-Cookie'].find{|c| c =~ /bar/ } or flunk('not found a session key')
+      /bar=(\S*)/ =~ bar_cookie && bar_id = $1 or flunk('not found a session id')
+      assert_equal({}, Marshal.load(@store.load(bar_id)))
+    end
+
+    def test_session_rollback
+      # create a cookie for session
+      @man.transaction(@req, @res) {|handler|
+        handler.new_session
+      }
+      assert_match(/session_id=\S*/, @res['Set-Cookie'])
+      /session_id=(\S*)/ =~ @res['Set-Cookie'] && id = $1 or flunk('not found a session id')
+
+      begin
+        @man.transaction(@req, @res) {|handler|
+          session = handler.new_session
+          session['foo'] = "Hello world.\n"
+          raise RuntimeError
+        }
+        flunk('not to reach')
+      rescue RuntimeError
+        # nothing to do.
+      end
+
+      assert_equal({}, Marshal.load(@store.load(id)))
+    end
+
+    def test_session_continue
+      @man.transaction(@req, @res) {|handler|
+        session = handler.new_session
+        session['foo'] = "Hello world.\n"
+      }
+
+      /session_id=\S*/ =~ @res['Set-Cookie'] or flunk('not found a session id')
+      req2 = Rack::Request.new({ 'HTTP_COOKIE' => $& })
+      res2 = Rack::Response.new
+
+      @man.transaction(req2, res2) {|handler|
+        session = handler.new_session
+        assert_equal({ 'foo' => "Hello world.\n" }, session)
+      }
+    end
+
+    def test_session_not_found
+      @man.transaction(@req, @res) {|handler|
+        assert_raise(Gluon::SessionNotFoundError) {
+          handler.new_session(false)
+        }
+      }
+    end
+
+    def test_session_with_options
+      @man.transaction(@req, @res) {|handler|
+        handler.new_session(true, :domain => 'www.foo.net', :path => '/')
+      }
+      assert_match(%r"session_id=\S+; domain=www.foo.net; path=/", @res['Set-Cookie'])
+    end
+  end
+
   class RequestResponseContextTest < Test::Unit::TestCase
     # for ident(1)
     CVS_ID = '$Id$'
