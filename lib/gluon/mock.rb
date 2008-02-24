936c4f365d8e05fb2677f7b7c6501d3e0d3a518c
toki <toki@d28d52c5-2bfd-4296-a7a2-2b3bd35462a5>
2008-02-24 14:21:04 +0000

    *** empty log message ***
    git-svn-id: file:///home/toki/svn_root/gluon/gluon/trunk@168 d28d52c5-2bfd-4296-a7a2-2b3bd35462a5

--- lib/gluon/mock.rb
+++ lib/gluon/mock.rb
@@ -1,5 +1,6 @@
-# mock session
+# mock
 
+require 'forwardable'
 require 'gluon/rs'
 require 'rack'
 
@@ -8,8 +9,12 @@ module Gluon
     # for ident(1)
     CVS_ID = '$Id$'
 
+    extend Forwardable
+
     def initialize(url_map=[])
       @dispatcher = Dispatcher.new(url_map)
+      @session_man = MockSessionManager.new
+      @session = nil
     end
 
     attr_reader :dispatcher
@@ -17,8 +22,44 @@ module Gluon
     def new_request(env)
       req = Rack::Request.new(env)
       res = Rack::Response.new
-      session = Object.new	# dummy
-      Gluon::RequestResponseContext.new(req, res, session, @dispatcher)
+      @session = @session_man.new_mock_session(req, res)
+      Gluon::RequestResponseContext.new(req, res, @session, @dispatcher)
+    end
+
+    def_delegator :@session, :get_for_mock, :session_get
+    def_delegator :@session, :session_next
+
+    def close_response
+      @session.save_all
+      @session = nil
+    end
+  end
+
+  class MockSessionManager < SessionManager
+    # for ident(1)
+    CVS_ID = '$Id$'
+
+    def initialize
+      super(:auto_expire => false,
+	    :store => MemoryStore.new)
+    end
+
+    def new_mock_session(req, res)
+      MockSessionHandler.new(self, req, res)
+    end
+  end
+
+  class MockSessionHandler < SessionHandler
+    # for ident(1)
+    CVS_ID = '$Id$'
+
+    def get_for_mock(options={})
+      get(false, options)
+    end
+
+    def next(env)
+      env['HTTP_COOKIE'] = @sessions.map{|key, (id, *others)| "#{key}=#{id}" }.join('; ')
+      env
     end
   end
 end
