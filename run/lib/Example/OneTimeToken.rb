e338777412e83da2f2eed2af581eda5e9a09061f
toki <toki@d28d52c5-2bfd-4296-a7a2-2b3bd35462a5>
2008-05-10 10:50:17 +0000

    2008-05-10  TOKI Yoshinori  <toki@freedom.ne.jp>
    	* lib/gluon/web/token.rb: one time token for web form.
    
    
    git-svn-id: file:///home/toki/svn_root/gluon/gluon/trunk@254 d28d52c5-2bfd-4296-a7a2-2b3bd35462a5

---
+++ run/lib/Example/OneTimeToken.rb
@@ -0,0 +1,50 @@
+require 'thread'
+
+class Example
+  class OneTimeToken
+    include Gluon::Web::OneTimeToken
+
+    attr_writer :c
+
+    class Count
+      def initialize
+	@lock = Mutex.new
+	@value = 0
+      end
+
+      def value
+	@lock.synchronize{ @value }
+      end
+
+      def succ!
+	@lock.synchronize{ @value += 1 }
+	self
+      end
+    end
+
+    COUNT = Count.new
+
+    def page_start
+      one_time_token_setup
+      @count = COUNT.value
+      @now = Time.now
+    end
+
+    attr_reader :count
+
+    def count_up
+      sleep(5)
+      COUNT.succ!
+      @count = COUNT.value
+    end
+
+    def now
+      @now.to_s
+    end
+  end
+end
+
+# Local Variables:
+# mode: Ruby
+# indent-tabs-mode: nil
+# End:
