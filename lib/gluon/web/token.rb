4c8ff8152b981600725223f3ba36a13819d836f1
toki <toki@d28d52c5-2bfd-4296-a7a2-2b3bd35462a5>
2008-06-04 14:24:07 +0000

    git-svn-id: file:///home/toki/svn_root/gluon/gluon/trunk@275 d28d52c5-2bfd-4296-a7a2-2b3bd35462a5

--- lib/gluon/web/token.rb
+++ lib/gluon/web/token.rb
@@ -48,36 +48,54 @@ module Gluon
         end
       end
 
+      # :stopdoc:
       COUNT = Hash.new{|hash, key|
         hash[key] = TokenCounter.new
       }
+      # :startdoc:
 
       attr_writer :c
       attr_reader :one_time_token
 
-      def one_time_token_setup
-        count = COUNT[self.class]
-        @one_time_token = TokenField.new(count)
-        @c.logger.debug("#{self}.one_time_token_setup(): (count,field) -> (#{count.value},#{@one_time_token.value})") if @c.logger.debug?
+      # template method for storage of counter. a block parameter of
+      # <em>count</em> is an instance of TokenCounter class.
+      def one_time_token_transaction # :yields: count
+        yield(COUNT[self.class])
       end
+      private :one_time_token_transaction
+
+      def one_time_token_setup
+        one_time_token_transaction{|count|
+          @one_time_token = TokenField.new(count)
+          @c.logger.debug("#{self}.one_time_token_setup(): (count,field) -> (#{count.value},#{@one_time_token.value})") if @c.logger.debug?
+        }
+        nil
+      end
+      private :one_time_token_setup
 
       def one_time_token_check
         unless (@one_time_token) then
           raise "need for call: #{OneTimeToken}\#one_time_token_setup"
         end
-        count = COUNT[self.class]
-        @c.logger.debug("#{self}.one_time_token_check(): (count,field) -> (#{count.value},#{@one_time_token.value})") if @c.logger.debug?
-        if (count.succ!(@one_time_token)) then
-          if (@c.logger.debug?) then
-            @c.logger.debug("#{self}.one_time_token_check() -> OK")
-            @c.logger.debug("#{self}.one_time_token_check(): (count,field) -> (#{count.value},#{@one_time_token.value})")
+
+        ret_val = nil
+        one_time_token_transaction{|count|
+          @c.logger.debug("#{self}.one_time_token_check(): (count,field) -> (#{count.value},#{@one_time_token.value})") if @c.logger.debug?
+          if (count.succ!(@one_time_token)) then
+            if (@c.logger.debug?) then
+              @c.logger.debug("#{self}.one_time_token_check() -> OK")
+              @c.logger.debug("#{self}.one_time_token_check(): (count,field) -> (#{count.value},#{@one_time_token.value})")
+            end
+            ret_val = true
+          else
+            @c.logger.debug("#{self}.one_time_token_check() -> NG") if @c.logger.debug?
+            ret_val = false
           end
-          true
-        else
-          @c.logger.debug("#{self}.one_time_token_check() -> NG") if @c.logger.debug?
-          false
-        end
+        }
+
+        ret_val
       end
+      private :one_time_token_check
 
       def page_check
         one_time_token_check
