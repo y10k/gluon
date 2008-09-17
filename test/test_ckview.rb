a76c3b2d2be279d9a4833639b67d142e131ace9d
toki <toki@d28d52c5-2bfd-4296-a7a2-2b3bd35462a5>
2008-09-17 16:25:27 +0000

    *** empty log message ***
    git-svn-id: file:///home/toki/svn_root/gluon/gluon/trunk@339 d28d52c5-2bfd-4296-a7a2-2b3bd35462a5

--- test/test_ckview.rb
+++ test/test_ckview.rb
@@ -51,6 +51,135 @@ module Gluon::Test
     def test_parse_attrs_empty
       assert_equal({}, Gluon::CKView.parse_attrs(''))
     end
+
+    def test_parse_text
+      assert_equal([ [ :text, "Hello world.\n" ] ],
+                   Gluon::CKView.parse("Hello world.\n"))
+    end
+
+    def test_parse_gluon_tag_single
+      assert_equal([ [ :gluon_tag_single, '<gluon />', {} ] ],
+                   Gluon::CKView.parse('<gluon />'))
+    end
+
+    def test_parse_gluon_tag_single_with_attrs
+      assert_equal([ [ :gluon_tag_single,
+                       '<gluon foo="Apple" bar="Banana" />',
+                       { 'foo' => 'Apple', 'bar' => 'Banana' }
+                     ]
+                   ],
+                   Gluon::CKView.parse('<gluon foo="Apple" bar="Banana" />'))
+    end
+
+    def test_parse_gluon_tag_single_sparsed
+      assert_equal([ [ :gluon_tag_single,
+                       "<\ngluon foo  =  'Apple'\tbar = 'Banana'\n/>",
+                       { 'foo' => 'Apple', 'bar' => 'Banana' }
+                     ]
+                   ],
+                   Gluon::CKView.parse("<\ngluon foo  =  'Apple'\tbar = 'Banana'\n/>"))
+    end
+
+    def test_parse_gluon_tag_single_densed
+      assert_equal([ [ :gluon_tag_single,
+                       '<gluon foo="Apple"bar="Banana"/>',
+                       { 'foo' => 'Apple', 'bar' => 'Banana' }
+                     ]
+                   ],
+                   Gluon::CKView.parse('<gluon foo="Apple"bar="Banana"/>'))
+    end
+
+    def test_parse_gluon_tag_start_end
+      assert_equal([ [ :gluon_tag_start, '<gluon>', {} ],
+                     [ :gluon_tag_end, '</gluon>' ]
+                   ],
+                   Gluon::CKView.parse('<gluon></gluon>'))
+    end
+
+    def test_parse_gluon_tag_start_end_with_attrs
+      assert_equal([ [ :gluon_tag_start,
+                       '<gluon foo="Apple" bar="Banana">',
+                       { 'foo' => 'Apple', 'bar' => 'Banana' }
+                     ],
+                     [ :gluon_tag_end, '</gluon>' ]
+                   ],
+                   Gluon::CKView.parse('<gluon foo="Apple" bar="Banana"></gluon>'))
+    end
+
+    def test_parse_gluon_tag_start_end_sparsed
+      assert_equal([ [ :gluon_tag_start,
+                       "<\ngluon foo  =  'Apple'\tbar = 'Banana'\n>",
+                       { 'foo' => 'Apple', 'bar' => 'Banana' }
+                     ],
+                     [ :gluon_tag_end, "</\n gluon  >" ]
+                   ],
+                   Gluon::CKView.parse("<\ngluon foo  =  'Apple'\tbar = 'Banana'\n></\n gluon  >"))
+    end
+
+    def test_parse_gluon_tag_start_end_densed
+      assert_equal([ [ :gluon_tag_start,
+                       '<gluon foo="Apple"bar="Banana">',
+                       { 'foo' => 'Apple', 'bar' => 'Banana' }
+                     ],
+                     [ :gluon_tag_end, '</gluon>' ]
+                   ],
+                   Gluon::CKView.parse('<gluon foo="Apple"bar="Banana"></gluon>'))
+    end
+
+    def test_parse_gluon_tag_all
+      text = "<html>\n"
+      text << "<head><title><gluon refkey=\"title\" /></title></head>\n"
+      text << "<body>\n"
+      text << "<h1><gluon refkey=\"title\" /></h1>\n"
+      text << "<p><gluon refkey=\"message\" /></p>\n"
+      text << "<gluon refkey=\"form?\">\n"
+      text << "  <form>\n"
+      text << "    <p>\n"
+      text << "      <label for=\"memo\">Memo:</label>\n"
+      text << "      <gluon refkey=\"memo\" id=\"memo\" />\n"
+      text << "      <gluon refkey=\"ok\" />\n"
+      text << "    </p>\n"
+      text << "  </form>\n"
+      text << "</gluon>\n"
+      text << "</body>\n"
+      text << "</html>\n"
+
+      assert_equal([ [ :text, "<html>\n<head><title>" ],
+                     [ :gluon_tag_single,
+                       "<gluon refkey=\"title\" />",
+                       { 'refkey' => 'title' }
+                     ],
+                     [ :text, "</title></head>\n<body>\n<h1>" ],
+                     [ :gluon_tag_single,
+                       "<gluon refkey=\"title\" />",
+                       { 'refkey' => 'title' }
+                     ],
+                     [ :text, "</h1>\n<p>" ],
+                     [ :gluon_tag_single,
+                       "<gluon refkey=\"message\" />",
+                       { 'refkey' => 'message' }
+                     ],
+                     [ :text, "</p>\n" ],
+                     [ :gluon_tag_start,
+                       "<gluon refkey=\"form?\">",
+                       { 'refkey' => 'form?' }
+                     ],
+                     [ :text, "\n  <form>\n    <p>\n      <label for=\"memo\">Memo:</label>\n      " ],
+                     [ :gluon_tag_single,
+                       "<gluon refkey=\"memo\" id=\"memo\" />",
+                       { 'refkey' => 'memo', 'id' => 'memo' }
+                     ],
+                     [ :text, "\n      " ],
+                     [ :gluon_tag_single,
+                       "<gluon refkey=\"ok\" />",
+                       { 'refkey' => 'ok' }
+                     ],
+                     [ :text, "\n    </p>\n  </form>\n" ],
+                     [ :gluon_tag_end, '</gluon>' ],
+                     [ :text, "\n</body>\n</html>\n" ]
+                   ],
+                   Gluon::CKView.parse(text))
+    end
   end
 end
 
