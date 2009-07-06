afba1a801b2ee405525b8b66600c089be79d097d
toki <toki@d28d52c5-2bfd-4296-a7a2-2b3bd35462a5>
2009-07-06 16:35:35 +0000

    syntax implementation and view syntax unit-test.
    git-svn-id: file:///home/toki/svn_root/gluon/gluon/branches/gluon2@584 d28d52c5-2bfd-4296-a7a2-2b3bd35462a5

--- lib/gluon/controller.rb
+++ lib/gluon/controller.rb
@@ -14,6 +14,8 @@ module Gluon
 
     # :stopdoc:
     PATH_FILTER = {}
+    VIEW_EXPORT = {}
+    FORM_EXPORT = {}
     # :startdoc:
 
     # = controller syntax
@@ -28,49 +30,149 @@ module Gluon
         nil
       end
 
+      def __gluon_export__(table, name, type, options)
+        name = name.to_sym
+        unless (public_method_defined? name) then
+          raise NoMethodError, "not defineid method `#{name}' of `#{self}'"
+        end
+        table[self] = {} unless (table.key? name)
+        table[self][name] = { :type => type, :options => options }
+        nil
+      end
+
       def gluon_value(name, options={})
+        __gluon_export__(VIEW_EXPORT, name, :value, options)
+      end
+
+      def gluon_value_reader(name, options={})
+        class_eval{ attr_reader(name) } # why is class_eval necessary?
+        gluon_value(name, options)
       end
 
       def gluon_cond(name, options={})
+        __gluon_export__(VIEW_EXPORT, name, :cond, options)
+      end
+
+      def gluon_cond_reader(name, options={})
+        class_eval{ attr_reader(name) } # why is class_eval necessary?
+        gluon_cond(name, options)
       end
 
       def gluon_foreach(name, options={})
+        __gluon_export__(VIEW_EXPORT, name, :foreach, options)
+        __gluon_export__(FORM_EXPORT, name, :foreach, options)
+      end
+
+      def gluon_foreach_reader(name, options={})
+        class_eval{ attr_reader(name) } # why is class_eval necessary?
+        gluon_foreach(name, options)
       end
 
       def gluon_link(name, options={})
+        __gluon_export__(VIEW_EXPORT, name, :link, options)
+      end
+
+      def gluon_link_reader(name, options={})
+        class_eval{ attr_reader(name) } # why is class_eval necessary?
+        gluon_link(name, options)
       end
 
       def gluon_action(name, options={})
+        __gluon_export__(VIEW_EXPORT, name, :action, options)
+      end
+
+      def gluon_action_reader(name, options={})
+        class_eval{ attr_reader(name) } # why is class_eval necessary?
+        gluon_action(name, options)
       end
 
       def gluon_frame(name, options={})
+        __gluon_export__(VIEW_EXPORT, name, :frame, options)
+      end
+
+      def gluon_frame_reader(name, options={})
+        class_eval{ attr_reader(name) } # why is class_eval necessary?
+        gluon_frame(name, options)
       end
 
       def gluon_import(name, options={}, &block)
+        options = { :block => block }.merge(options)
+        __gluon_export__(VIEW_EXPORT, name, :import, options)
+        __gluon_export__(FORM_EXPORT, name, :import, options)
+      end
+
+      def gluon_import_reader(name, options={}, &block)
+        class_eval{ attr_reader(name) } # why is class_eval necessary?
+        gluon_import(name, options, &block)
       end
 
       def gluon_text(name, options={})
+        __gluon_export__(FORM_EXPORT, name, :text, options)
+      end
+
+      def gluon_text_accessor(name, options={})
+        class_eval{ attr_accessor(name) } # why is class_eval necessary?
+        gluon_text(name, options)
       end
 
       def gluon_passwd(name, options={})
+        __gluon_export__(FORM_EXPORT, name, :passwd, options)
+      end
+
+      def gluon_passwd_accessor(name, options={})
+        class_eval{ attr_accessor(name) } # why is class_eval necessary?
+        gluon_passwd(name, options)
       end
 
       def gluon_submit(name, options={})
+        __gluon_export__(FORM_EXPORT, name, :submit, options)
       end
 
       def gluon_hidden(name, options={})
+        __gluon_export__(FORM_EXPORT, name, :hidden, options)
+      end
+
+      def gluon_hidden_accessor(name, options={})
+        class_eval{ attr_accessor(name) } # why is class_eval necessary?
+        gluon_hidden(name, options)
       end
 
       def gluon_checkbox(name, options={})
+        __gluon_export__(FORM_EXPORT, name, :checkbox, options)
+      end
+
+      def gluon_checkbox_accessor(name, options={})
+        class_eval{ attr_accessor(name) } # why is class_eval necessary?
+        gluon_checkbox(name, options)
       end
 
       def gluon_radio(name, list, options={})
+        options = { :list => list }.merge(options)
+        __gluon_export__(FORM_EXPORT, name, :radio, options)
+      end
+
+      def gluon_radio_accessor(name, list, options={})
+        class_eval{ attr_accessor(name) } # why is class_eval necessary?
+        gluon_radio(name, list, options)
       end
 
       def gluon_select(name, list, options={})
+        options = { :list => list }.merge(options)
+        __gluon_export__(FORM_EXPORT, name, :select, options)
+      end
+
+      def gluon_select_accessor(name, list, options={})
+        class_eval{ attr_accessor(name) } # why is class_eval necessary?
+        gluon_select(name, list, options)
       end
 
       def gluon_textarea(name, options={})
+        __gluon_export__(FORM_EXPORT, name, :textarea, options)
+      end
+
+      def gluon_textarea_accessor(name, options={})
+        class_eval{ attr_accessor(name) } # why is class_eval necessary?
+        gluon_textarea(name, options)
       end
     end
 
@@ -86,6 +188,26 @@ module Gluon
 
       def find_path_block(page_type)
         entry = PATH_FILTER[page_type] and return entry[:block]
+      end
+
+      def find_export(table, page_type)
+        export = {}
+        for module_or_class in page_type.ancestors
+          if (table.key? module_or_class) then
+            export.update(table[module_or_class])
+          end
+        end
+
+        export
+      end
+      private :find_export
+
+      def find_view_export(page_type)
+        find_export(VIEW_EXPORT, page_type)
+      end
+
+      def find_form_export(page_type)
+        find_export(FORM_EXPORT, page_type)
       end
     end
   end
--- test/test_controller.rb
+++ test/test_controller.rb
@@ -38,6 +38,119 @@ module Gluon::Test
       subclass = Class.new(@c)
       assert_nil(Gluon::Controller.find_path_block(subclass))
     end
+
+    def test_gluon_value
+      @c.class_eval{ gluon_value_reader :foo }
+      assert(entry = Gluon::Controller.find_view_export(@c))
+      assert_equal(:value, entry[:foo][:type])
+      assert_equal(true, (@c.public_method_defined? :foo))
+      assert_equal(false, (@c.public_method_defined? :foo=))
+    end
+
+    def test_gluon_value_inherited
+      @c.class_eval{ gluon_value_reader :foo }
+      subclass = Class.new(@c)
+      assert(entry = Gluon::Controller.find_view_export(subclass))
+      assert_equal(:value, entry[:foo][:type])
+    end
+
+    def test_gluon_cond
+      @c.class_eval{ gluon_cond_reader :foo }
+      assert(entry = Gluon::Controller.find_view_export(@c))
+      assert_equal(:cond, entry[:foo][:type])
+      assert_equal(true, (@c.public_method_defined? :foo))
+      assert_equal(false, (@c.public_method_defined? :foo=))
+    end
+
+    def test_gluon_cond_inherited
+      @c.class_eval{ gluon_cond_reader :foo }
+      subclass = Class.new(@c)
+      assert(entry = Gluon::Controller.find_view_export(subclass))
+      assert_equal(:cond, entry[:foo][:type])
+    end
+
+    def test_gluon_foreach
+      @c.class_eval{ gluon_foreach_reader :foo }
+      assert(view_entry = Gluon::Controller.find_view_export(@c))
+      assert_equal(:foreach, view_entry[:foo][:type])
+      assert(form_entry = Gluon::Controller.find_form_export(@c))
+      assert_equal(:foreach, form_entry[:foo][:type])
+      assert_equal(true, (@c.public_method_defined? :foo))
+      assert_equal(false, (@c.public_method_defined? :foo=))
+    end
+
+    def test_gluon_foreach_inherited
+      @c.class_eval{ gluon_foreach_reader :foo }
+      subclass = Class.new(@c)
+      assert(view_entry = Gluon::Controller.find_view_export(subclass))
+      assert_equal(:foreach, view_entry[:foo][:type])
+      assert(form_entry = Gluon::Controller.find_form_export(subclass))
+      assert_equal(:foreach, form_entry[:foo][:type])
+    end
+
+    def test_gluon_link
+      @c.class_eval{ gluon_link_reader :foo }
+      assert(entry = Gluon::Controller.find_view_export(@c))
+      assert_equal(:link, entry[:foo][:type])
+      assert_equal(true, (@c.public_method_defined? :foo))
+      assert_equal(false, (@c.public_method_defined? :foo=))
+    end
+
+    def test_gluon_link_inherited
+      @c.class_eval{ gluon_link_reader :foo }
+      subclass = Class.new(@c)
+      assert(entry = Gluon::Controller.find_view_export(subclass))
+      assert_equal(:link, entry[:foo][:type])
+    end
+
+    def test_gluon_action
+      @c.class_eval{ gluon_action_reader :foo }
+      assert(entry = Gluon::Controller.find_view_export(@c))
+      assert_equal(:action, entry[:foo][:type])
+      assert_equal(true, (@c.public_method_defined? :foo))
+      assert_equal(false, (@c.public_method_defined? :foo=))
+    end
+
+    def test_gluon_action_inherited
+      @c.class_eval{ gluon_action_reader :foo }
+      subclass = Class.new(@c)
+      assert(entry = Gluon::Controller.find_view_export(subclass))
+      assert_equal(:action, entry[:foo][:type])
+    end
+
+    def test_gluon_frame
+      @c.class_eval{ gluon_frame_reader :foo }
+      assert(entry = Gluon::Controller.find_view_export(@c))
+      assert_equal(:frame, entry[:foo][:type])
+      assert_equal(true, (@c.public_method_defined? :foo))
+      assert_equal(false, (@c.public_method_defined? :foo=))
+    end
+
+    def test_gluon_frame_inherited
+      @c.class_eval{ gluon_frame_reader :foo }
+      subclass = Class.new(@c)
+      assert(entry = Gluon::Controller.find_view_export(subclass))
+      assert_equal(:frame, entry[:foo][:type])
+    end
+
+    def test_gluon_import
+      @c.class_eval{ gluon_import_reader :foo }
+      assert(view_entry = Gluon::Controller.find_view_export(@c))
+      assert_equal(:import, view_entry[:foo][:type])
+      assert(form_entry = Gluon::Controller.find_form_export(@c))
+      assert_equal(:import, form_entry[:foo][:type])
+      assert_equal(true, (@c.public_method_defined? :foo))
+      assert_equal(false, (@c.public_method_defined? :foo=))
+    end
+
+    def test_gluon_import_inherited
+      @c.class_eval{ gluon_import_reader :foo }
+      subclass = Class.new(@c)
+      assert(view_entry = Gluon::Controller.find_view_export(subclass))
+      assert_equal(:import, view_entry[:foo][:type])
+      assert(form_entry = Gluon::Controller.find_form_export(subclass))
+      assert_equal(:import, form_entry[:foo][:type])
+    end
   end
 end
 
