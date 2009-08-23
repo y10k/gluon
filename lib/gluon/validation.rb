3e4dccc106fadb5317725f9b30c5626eae19dd61
toki <toki@d28d52c5-2bfd-4296-a7a2-2b3bd35462a5>
2009-08-23 05:03:06 +0000

    validation utility.
    git-svn-id: file:///home/toki/svn_root/gluon/gluon/branches/gluon2@766 d28d52c5-2bfd-4296-a7a2-2b3bd35462a5

--- lib/gluon/validation.rb
+++ lib/gluon/validation.rb
@@ -1,353 +1,81 @@
+# -*- coding: utf-8 -*-
 # = gluon - simple web application framework
-#
-# Author:: $Author$
-# Date:: $Date$
-# Revision:: $Revision$
-#
 # == license
 #   :include:../LICENSE
 #
 
-require 'forwardable'
+require 'gluon/controller'
 
 module Gluon
   class Validator
     # for ident(1)
     CVS_ID = '$Id$'
 
-    module PrintError
-      def print_error(message)
-        if (@errors) then
-          @errors << message
-        end
-        nil
-      end
-      private :print_error
-    end
-    include PrintError
-
-    class Checker
-      include PrintError
-
-      def initialize(results, name, value, errors=nil)
-        @results = results
-        @name = name
-        @value = value
-        @errors = errors
-      end
-
-      def validation_name
-        @name
-      end
-
-      def validation_value
-        @value
-      end
-
-      def validation_type
-        self.class.type_name
-      end
-    end
-
-    class Scalar < Checker
-      class << self
-        def type_name
-          'scalar'
-        end
-
-        def check_type?(value)
-          value.is_a? String
-        end
-      end
-
-      def validate_scalar(error_message=nil, negate=false)
-        r = yield(@value)
-        r = ! r if negate
-        if (r) then
-          @results << true
-        else
-          @results << false
-          print_error(error_message ||
-                      "value at `#{@name}' is invalid.")
-        end
-
-        nil
-      end
-
-      alias validate validate_scalar
-
-      def match_scalar(regexp, error_message=nil, negate=false)
-        nt = (negate) ? ' not' : ''
-        emsg = error_message ||
-          "value at `#{@name}' should#{nt} be match to `#{regexp}'."
-        validate_scalar(emsg, negate) {
-          regexp === @value
-        }
-      end
-
-      alias match match_scalar
-
-      def not_match_scalar(regexp, error_message=nil)
-        match_scalar(regexp, error_message, true)
-      end
-
-      alias not_match not_match_scalar
-
-      def range_scalar(range, error_message=nil, negate=false)
-        nt = (negate) ? ' not' : ''
-        emsg = error_message ||
-          "value at `#{@name}' is#{nt} out of range `#{range}'.",
-        validate_scalar(emsg, negate) {
-          if (block_given?) then
-            v = yield(@value)
-          else
-            v = @value
-          end
-          range.include? v
-        }
-      end
-
-      alias range range_scalar
-
-      def not_range_scalar(range, error_message=nil)
-        range_scalar(range, error_message, true)
-      end
-
-      alias not_range not_range_scalar
-    end
-
-    class List < Checker
-      class << self
-        def type_name
-          'list'
-        end
-
-        def check_type?(value)
-          value.is_a? Array
-        end
-      end
-
-      def _validate_list(list_validator, error_message=nil, negate=false)
-        if (negate) then
-          validate = proc{|v| ! yield(v) }
-        else
-          validate = proc{|v| yield(v) }
-        end
-
-        if (@value.__send__(list_validator, &validate)) then
-          @results << true
-        else
-          @results << false
-          print_error(error_message ||
-                      "list at `#{@name}' is invalid.")
-        end
-
-        nil
-      end
-      private :_validate_list
-
-      def validate_list_all(error_message=nil, negate=false)
-        _validate_list(:all?,
-                       error_message ||
-                       "all value of list at `@name' is invalid.",
-                       negate)
-      end
-
-      alias validate_list validate_list_all
-      alias validate validate_list
-
-      def match_list_all(regexp, error_message=nil, negate=false)
-        nt = (negate) ? ' not' : ''
-        emsg = error_message ||
-          "all value of list at `#{@name}' should#{nt} be match to `#{regexp}'.",
-        validate_list_all(emsg, negate) {|v|
-          regexp === v
-        }
-      end
-
-      alias match_list match_list_all
-      alias match match_list
-
-      def not_match_list_all(regexp, error_message=nil)
-        match_list_all(regexp, error_message, true)
-      end
-
-      alias not_match_list not_match_list_all
-      alias not_match not_match_list
-
-      def range_list_all(range, error_message=nil, negate=false)
-        nt (negate) ? ' not' : ''
-        emsg = error_message ||
-          "all value at `#{@name}' is#{nt} out of range `#{range}'."
-        validate_list_all(emsg, negate) {|v|
-          if (block_given?) then
-            v = yield(v)
-          end
-          range.include? v
-        }
-      end
-
-      alias range_list range_list_all
-      alias range range_list
-
-      def not_range_list_all(range, error_message=nil)
-        range_list_all(range, error_message, true)
-      end
-
-      alias not_range_list not_range_list_all
-      alias not_range not_range_list
-
-      def validate_list_any(error_message=nil, negate=false)
-        _validate_list(:any?,
-                       error_message ||
-                       "any value of list at `@name' is invalid.",
-                       negate)
-      end
-
-      def match_list_any(regexp, error_message=nil, negate=false)
-        nt = (negate) ? ' not' : ''
-        emsg = error_message ||
-          "any value of list at `#{@name}' should#{nt} be match to `#{regexp}'.",
-        validate_list_any(emsg, negate) {|v|
-          regexp === v
-        }
-      end
-
-      def not_match_list_any(regexp, error_message=nil)
-        match_list_any(regexp, error_message, true)
-      end
-
-      def range_list_any(range, error_message=nil, negate=false)
-        nt (negate) ? ' not' : ''
-        emsg = error_message ||
-          "any value at `#{@name}' is#{nt} out of range `#{range}'."
-        validate_list_any(emsg, negate) {|v|
-          if (block_given?) then
-            v = yield(v)
-          end
-          range.include? v
-        }
-      end
-
-      def not_range_list_any(range, error_message=nil)
-        range_list_any(range, error_message, true)
-      end
-    end
-
-    class Bool < Checker
-      class << self
-        def type_name
-          'bool'
-        end
-
-        def check_type?(value)
-          case (value)
-          when TrueClass, FalseClass
-            true
-          else
-            false
-          end
-        end
-      end
-    end
-
-    def initialize(controller, errors=nil)
-      @controller = controller
+    def initialize(controller, errors, prefix='')
+      @c = controller
       @errors = errors
-      @optional = false
-      @results = []
+      @prefix = prefix
+      @fail_count = 0
+      @form_export = Controller.find_form_export(@c.class)
     end
 
-    def optional_with(new_optional)
-      if (block_given?) then
-        save_optional = @optional
-        begin
-          @optional = new_optional
-          r = yield
-        ensure
-          @optional = save_optional
-        end
-        r
-      else
-        @optional = new_optional
-        nil
-      end
-    end
-    private :optional_with
-
-    def validate_with(name, checker, error_message=nil)
-      value = @controller.__send__(name)
-      if (value.nil?) then
-        if (@optional) then
-          @results << true
-        else
-          @results << false
-          print_error(error_message ||
-                      "`#{@value}' is not defined at `#{@name}'.")
-        end
-      elsif (checker.check_type? value) then
-        @results << true
-        if (block_given?) then
-          yield(checker.new(@results, name, value, @errors))
-        end
-      else
-        @results << false
-        print_error(error_message ||
-                    "`#{@value}' is not #{checker.type_name} at `#{@name}'.")
-      end
-
-      nil
-    end
-    private :validate_with
-
-    def optional(&block)
-      optional_with(true, &block)
-    end
-
-    def required(&block)
-      optional_with(false, &block)
-    end
-
-    def scalar(name, error_message=nil, &block)
-      validate_with(name, Scalar, error_message, &block)
-    end
-
-    def list(name, error_message=nil, &block)
-      validate_with(name, List, error_message, &block)
-    end
-
-    def bool(name, error_message=nil, &block)
-      validate_with(name, Bool, error_message, &block)
-    end
-
-    def validate_one_time_token(error_message=nil)
-      if (@controller.one_time_token_valid?) then
-        @results << true
-      else
-        @results << false
-        if (@errors) then
-          @errors << (error_message || 'Not reload form.')
-        end
-      end
-      nil
+    def controller
+      @c
     end
 
     def validate(error_message)
-      if (yield) then
-        @results << true
-      else
-        @results << false
-        if (@errors) then
-          @errors << error_message
-        end
+      unless (yield) then
+        @fail_count += 1
+        @errors << error_message
       end
-      nil
+
+      self
     end
 
-    def validation
-      @results.clear
-      yield(self)
-      @results.all?
+    def validated?
+      @fail_count == 0
+    end
+
+    def foreach(name)
+      if (form_entry = @form_export[name]) then
+        if (:foreach == form_entry[:type]) then
+          if (list = @c.__send__(name)) then
+            list.each_with_index do |c, i|
+              v = Validator.new(c, @errors, "#{@prefix}#{name}(#{i}).")
+              yield(v)
+              unless (v.validated?) then
+                @fail_count += 1
+              end
+            end
+          end
+        else
+          raise "expected foreach, but was #{form_entry[:type]} of `#{name}' for `#{@c}'"
+        end
+      else
+        raise "not found a form export of `#{name}' for `#{@c}'"
+      end
+
+      self
+    end
+
+    def import(name)
+      if (form_entry = @form_export[name]) then
+        if (:import == form_entry[:type]) then
+          if (c = @c.__send__(name)) then
+            v = Validator.new(c, @errors, "#{@prefix}#{name}.")
+            yield(v)
+            unless (v.validated?) then
+              @fail_count += 1
+            end
+          end
+        else
+          raise "expected import, but was #{form_entry[:type]} of `#{name}' for `#{@c}'"
+        end
+      else
+        raise "not found a form export of `#{name}' for `#{@c}'"
+      end
+
+      self
     end
   end
 
@@ -355,129 +83,18 @@ module Gluon
     # for ident(1)
     CVS_ID = '$Id$'
 
-    module Syntax
-      %w[ optional required validate_one_time_token ].each do |name|
-        module_eval(<<-EOF, "#{__FILE__},#{Syntax}\##{name}", __LINE__ + 1)
-          def #{name}(*args, &block)
-            if (@__gluon_checker__) then
-              raise %q"not in validation checker block for `#{name}'."
-            end
-            if (@__gluon_validator__) then
-              @__gluon_validator__.#{name}(*args, &block)
-            else
-              super
-            end
-          end
-        EOF
-      end
-
-      %w[ scalar list bool ].each do |name|
-        module_eval(<<-EOF, "#{__FILE__}: #{Syntax}\##{name}", __LINE__ + 1)
-          def #{name}(*args)
-            if (@__gluon_validator__) then
-              if (@__gluon_checker__) then
-                raise %q"not nested validation checker block for `#{name}'."
-              end
-              if (block_given?) then
-                @__gluon_validator__.#{name}(*args) {|checker|
-                  @__gluon_checker__ = checker
-                  begin
-                    r = yield
-                  ensure
-                    @__gluon_checker__ = nil
-                  end
-                  r
-                }
-              else
-                @__gluon_validator__.#{name}(*args)
-              end
-            else
-              super
-            end
-          end
-        EOF
-      end
-
-      %w[
-        validation_name
-        validation_value
-        validation_type
-
-        match not_match
-        range not_range
-
-        validate_scalar
-        match_scalar not_match_scalar
-        range_scalar not_range_scalar
-
-        validate_list
-        match_list not_match_list
-        range_list not_range_list
-
-        validate_list_all
-        match_list_all not_match_list_all
-        range_list_all not_range_list_all
-
-        validate_list_any
-        match_list_any not_match_list_any
-        range_list_any not_range_list_any
-      ].each do |name|
-        module_eval(<<-EOF, "#{__FILE__}: #{Syntax}\##{name}", __LINE__ + 1)
-          def #{name}(*args, &block)
-            if (@__gluon_checker__) then
-              @__gluon_checker__.#{name}(*args, &block)
-            else
-              super
-            end
-          end
-        EOF
-      end
-
-      def validate(*args, &block)
-        if (@__gluon_validator__) then
-          if (@__gluon_checker__) then
-            @__gluon_checker__.validate(*args, &block)
-          else
-            @__gluon_validator__.validate(*args, &block)
-          end
-        else
-          super
-        end
+    def self.included(controller_class)
+      if (! (controller_class.is_a? Class) || ! (controller_class.include? Controller)) then
+        raise "not a controller class of `#{controller_class}'"
       end
     end
 
-    def validation(errors=nil)
-      r = Validator.new(self, errors).validation{|validator|
-        if (@__gluon_validator__) then
-          raise 'not nested validation.'
-        end
-        @__gluon_validator__ = validator
-        begin
-          class << self
-            unless (include? Syntax) then
-              include Syntax
-            end
-          end
-          r = yield
-        ensure
-          @__gluon_validator__ = nil
-        end
-        r
-      }
-
-      if (@c.validation.nil?) then
-        @c.validation = r
-      else
-        if (@c.validation) then
-          @c.validation = r
-        else
-          # violation by previous validation check.
-        end
-      end
-
-      nil
+    def validation(errors)
+      v = Validator.new(self, errors, '')
+      yield(v)
+      @r.validation = v.validated?
+      self
     end
-    private :validation
   end
 end
 
--- test/test_validation.rb
+++ test/test_validation.rb
@@ -1,4 +1,5 @@
 #!/usr/local/bin/ruby
+# -*- coding: utf-8 -*-
 
 require 'gluon'
 require 'rack'
@@ -9,340 +10,206 @@ module Gluon::Test
     # for ident(1)
     CVS_ID = '$Id$'
 
-    include Gluon::Validation
-
-    attr_reader :foo
-
     def setup
-      @env = Rack::MockRequest.env_for('http://foo:8080/bar.cgi')
-      @mock = Gluon::Mock.new
-      @c = @mock.new_request(@env)
-      @foo = nil
+      @Controller = Class.new{
+        include Gluon::Controller
+        include Gluon::Validation
+      }
+      @c = @Controller.new
+      @env = Rack::MockRequest.env_for('http://www.foo.com/run.cgi')
+      @r = Gluon::RequestResponseContext.new(Rack::Request.new(@env), Rack::Response.new)
+      @c.r = @r
       @errors = []
     end
 
-    def test_out_of_validation_block
-      validation(@errors) do    # extend Validation::Syntax
-      end
-
-      # for validation scope
-      assert_raise(NoMethodError) { optional }
-      assert_raise(NoMethodError) { required }
-      assert_raise(NoMethodError) { validate_one_time_token }
-      assert_raise(NoMethodError) { scalar }
-      assert_raise(NoMethodError) { list }
-      assert_raise(NoMethodError) { bool }
-      assert_raise(NoMethodError) { validate }
-
-      # for checker scope
-      assert_raise(NoMethodError) { match }
-      assert_raise(NoMethodError) { range }
-      assert_raise(NoMethodError) { validate }
-    end
-
-    def test_out_of_checker_block
-      validation(@errors) do
-        assert_raise(NoMethodError) { match }
-        assert_raise(NoMethodError) { range }
-        #assert_raise(NoMethodError) { validate }
-      end
-    end
-
-    def test_in_checker_block
-      @foo = 'HALO'
-      validation(@errors) do
-        scalar :foo do
-          assert_raise(RuntimeError) { optional }
-          assert_raise(RuntimeError) { required }
-          assert_raise(RuntimeError) { validate_one_time_token }
-          assert_raise(RuntimeError) { scalar }
-          assert_raise(RuntimeError) { list }
-          assert_raise(RuntimeError) { bool }
-        end
-      end
-    end
-
-    def test_validation_empty_ok
-      validation(@errors) do
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_ok
-      @foo = 'HALO'
-      validation(@errors) do
-        scalar :foo
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_required_block_ok
-      @foo = 'HALO'
-      validation(@errors) do
-        required do
-          scalar :foo
-        end
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_required_block_ng
-      validation(@errors) do
-        required do
-          scalar :foo
-        end
-      end
-      assert_equal(false, @c.validation)
-      assert_equal(1, @errors.length)
-    end
-
-    def test_validation_scalar_required_block_ok
-      @foo = 'HALO'
-      validation(@errors) do
-        required
-        scalar :foo
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_required_block_ng
-      validation(@errors) do
-        required
-        scalar :foo
-      end
-      assert_equal(false, @c.validation)
-      assert_equal(1, @errors.length)
-    end
-
-    def test_validation_scalar_optional_block_no_value_ok
-      validation(@errors) do
-        optional do
-          scalar :foo
-        end
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_optional_block_a_value_ok
-      @foo = 'HALO'
-      validation(@errors) do
-        optional do
-          scalar :foo
-        end
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_optional_block_a_value_ng
-      @foo = 'HALO'
-      validation(@errors) do
-        optional do
-          scalar :foo do
-            match /Z/
-          end
-        end
-      end
-      assert_equal(false, @c.validation)
-      assert_equal(1, @errors.length)
-    end
-
-    def test_validation_scalar_ng
-      @foo = nil
-      validation(@errors) do
-        scalar :foo do
-          # nothing to do.
-        end
-      end
-      assert_equal(false, @c.validation)
-      assert_equal(1, @errors.length)
-    end
-
-    def test_validation_scalar_match_ok
-      @foo = 'HALO'
-      validation(@errors) do
-        scalar :foo do
-          match /H/
-        end
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_match_ng
-      @foo = 'HALO'
-      validation(@errors) do
-        scalar :foo do
-          match /Z/
-        end
-      end
-      assert_equal(false, @c.validation)
-      assert_equal(1, @errors.length)
-    end
-
-    def test_validation_scalar_range_string_ok
-      @foo = 'bb'
-      validation(@errors) do
-        scalar :foo do
-          range 'a'..'z'
-        end
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_range_string_ng
-      @foo = 'bb'
-      validation(@errors) do
-        scalar :foo do
-          range 'c'..'z'
-        end
-      end
-      assert_equal(false, @c.validation)
-      assert_equal(1, @errors.length)
-    end
-
-    def test_validation_scalar_range_integer_ok
-      @foo = '5'
-      validation(@errors) do
-        scalar :foo do
-          range 1..10 do |v|
-            v.to_i
-          end
-        end
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_range_integer_ng
-      @foo = '5'
-      validation(@errors) do
-        scalar :foo do
-          range 1...5 do |v|
-            v.to_i
-          end
-        end
-      end
-      assert_equal(false, @c.validation)
-      assert_equal(1, @errors.length)
-    end
-
-    def test_validation_scalar_validate_ok
-      @foo = 'apple'
-      validation(@errors) do
-        scalar :foo do
-          validate do |v|
-            %w[ apple banana orange ].include? v
-          end
-        end
-      end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
-    end
-
-    def test_validation_scalar_validate_ng
-      @foo = 'pineapple'
-      validation(@errors) do
-        scalar :foo do
-          validate do |v|
-            %w[ apple banana orange ].include? v
-          end
-        end
-      end
-      assert_equal(false, @c.validation)
-      assert_equal(1, @errors.length)
-    end
-
-    def test_validation_validate_ok
-      validation(@errors) do
-        validate 'any ok' do
+    def test_validate_OK
+      @c.validation(@errors) do |v|
+        v.validate 'test error.' do
           true
         end
       end
-      assert_equal(true, @c.validation)
-      assert_equal(0, @errors.length)
+
+      assert_equal(true, @r.validation)
+      assert_equal([], @errors)
     end
 
-    def test_validation_validate_ng
-      validation(@errors) do
-        validate 'any ng' do
+    def test_validate_NG
+      @c.validation(@errors) do |v|
+        v.validate 'test error.' do
           false
         end
       end
-      assert_equal(false, @c.validation)
-      assert_equal(1, @errors.length)
-      assert_equal('any ng', @errors[0])
-    end
-  end
 
-  class ValidationTest_method_override < ValidationTest
-    def optional
-      raise 'optional'
+      assert_equal(false, @r.validation)
+      assert_equal([ 'test error.' ], @errors)
     end
 
-    def required
-      raise 'required'
-    end
-
-    def validate_one_time_token
-      raise 'validate_one_time_token'
-    end
-
-    def scalar
-      raise 'scalar'
-    end
-
-    def list
-      raise 'list'
-    end
-
-    def bool
-      raise 'bool'
-    end
-
-    def match
-      raise 'match'
-    end
-
-    def range
-      raise 'range'
-    end
-
-    def validate
-      raise 'validate'
-    end
-
-    def test_out_of_validation_block
-      validation(@errors) do    # extend Validation::Syntax
+    def test_validate_NG_2
+      @c.validation(@errors) do |v|
+        v.validate 'test error-1.' do
+          true
+        end
+        v.validate 'test error-2.' do
+          false
+        end
+        v.validate 'test error-3.' do
+          true
+        end
       end
 
-      # for validation scope
-      assert_raise(RuntimeError) { optional }
-      assert_raise(RuntimeError) { required }
-      assert_raise(RuntimeError) { validate_one_time_token }
-      assert_raise(RuntimeError) { scalar }
-      assert_raise(RuntimeError) { list }
-      assert_raise(RuntimeError) { bool }
-      assert_raise(RuntimeError) { validate }
-
-      # for checker scope
-      assert_raise(RuntimeError) { match }
-      assert_raise(RuntimeError) { range }
-      assert_raise(RuntimeError) { validate }
+      assert_equal(false, @r.validation)
+      assert_equal([ 'test error-2.' ], @errors)
     end
 
-    def test_out_of_checker_block
-      validation(@errors) do
-        assert_raise(RuntimeError) { match }
-        assert_raise(RuntimeError) { range }
-        #assert_raise(RuntimeError) { validate }
+    def test_foreach_validate_OK
+      @Controller.class_eval{
+        attr_writer :foo
+        gluon_foreach_reader :foo
+      }
+
+      component = Class.new{
+        def initialize(name, bar)
+          @name = name
+          @bar = bar
+        end
+
+        attr_reader :name
+
+        def bar?
+          @bar
+        end
+      }
+
+      @c.foo = [
+        component.new('a', true),
+        component.new('b', true),
+        component.new('c', true)
+      ]
+
+      @c.validation(@errors) do |v|
+        v.foreach :foo do |v|
+          v.validate "test error: #{v.controller.name}" do
+            v.controller.bar?
+          end
+        end
       end
+
+      assert_equal(true, @r.validation)
+      assert_equal([], @errors)
+    end
+
+    def test_foreach_validate_NG
+      @Controller.class_eval{
+        attr_writer :foo
+        gluon_foreach_reader :foo
+      }
+
+      component = Class.new{
+        def initialize(name, bar)
+          @name = name
+          @bar = bar
+        end
+
+        attr_reader :name
+
+        def bar?
+          @bar
+        end
+      }
+
+      @c.foo = [
+        component.new('a', true),
+        component.new('b', false),
+        component.new('c', true)
+      ]
+
+      @c.validation(@errors) do |v|
+        v.foreach :foo do |v|
+          v.validate "test error: #{v.controller.name}" do
+            v.controller.bar?
+          end
+        end
+      end
+
+      assert_equal(false, @r.validation)
+      assert_equal([ 'test error: b' ], @errors)
+    end
+
+    def test_foreach_validate_NG_all
+      @Controller.class_eval{
+        attr_writer :foo
+        gluon_foreach_reader :foo
+      }
+
+      component = Class.new{
+        def initialize(name, bar)
+          @name = name
+          @bar = bar
+        end
+
+        attr_reader :name
+
+        def bar?
+          @bar
+        end
+      }
+
+      @c.foo = [
+        component.new('a', false),
+        component.new('b', false),
+        component.new('c', false)
+      ]
+
+      @c.validation(@errors) do |v|
+        v.foreach :foo do |v|
+          v.validate "test error: #{v.controller.name}" do
+            v.controller.bar?
+          end
+        end
+      end
+
+      assert_equal(false, @r.validation)
+      assert_equal([ 'test error: a', 'test error: b', 'test error: c' ], @errors)
+    end
+
+    def test_import_validate_OK
+      @Controller.class_eval{
+        attr_writer :foo
+        gluon_import_reader :foo
+      }
+      component = Class.new
+      @c.foo = component.new
+
+      @c.validation(@errors) do |v|
+        v.import :foo do |v|
+          v.validate 'test error.' do
+            true
+          end
+        end
+      end
+
+      assert_equal(true, @r.validation)
+      assert_equal([], @errors)
+    end
+
+    def test_import_validate_NG
+      @Controller.class_eval{
+        attr_writer :foo
+        gluon_import_reader :foo
+      }
+      component = Class.new
+      @c.foo = component.new
+
+      @c.validation(@errors) do |v|
+        v.import :foo do |v|
+          v.validate 'test error.' do
+            false
+          end
+        end
+      end
+
+      assert_equal(false, @r.validation)
+      assert_equal([ 'test error.' ], @errors)
     end
   end
 end
